538e5a1a0d454b8a9839504950ee3bdb98570792
zillulofficial <zillul.hakim100@gmail.com>
2024-12-14 12:55:40 +0600

    users related API like get and post method created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt= require('jsonwebtoken')
 const cookieParser = require('cookie-parser');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.port || 9000
 const app = express()
@@ -32,9 +32,28 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     
+    const userCollection = client.db('BistroBoss').collection('user')
     const menuCollection = client.db('BistroBoss').collection('menu')
     const reviewCollection = client.db('BistroBoss').collection('review')
     const cartCollection = client.db('BistroBoss').collection('cart')
+
+    // user related API
+    app.get('/users', async(req, res)=>{
+      const result= await userCollection.find().toArray()
+      res.send(result)
+    })
+    app.post('/users', async(req, res)=>{
+      const user= req.body
+      // insert email if it doesn't exist
+      // it can be done in various ways (email unique, upsert, simple checking)
+      const query= {email: user.email}
+      const existingUser= await userCollection.findOne(query)
+      if(existingUser){
+        return res.send({message: "user already exists", insertedId: null})
+      }
+      const result= await userCollection.insertOne(user)
+      res.send(result)
+    })
 
     // menu related API
     app.get('/menu', async(req, res)=>{
@@ -61,6 +80,12 @@ async function run() {
       const query= {email: email}
       const result = await cartCollection.find(query).toArray()
       res.send(result)
+  })
+  app.delete('/carts/:id', async(req, res)=>{
+    const id = req.params.id
+    const query= {_id: new ObjectId(id)}
+    const result= await cartCollection.deleteOne(query)
+    res.send(result)
   })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
