1f7057172904e78e338292c64b042eaf4436ecf8
zillulofficial <zillul.hakim100@gmail.com>
2024-12-15 11:37:34 +0600

    user delete method and update admin status implemented

--- index.js
+++ index.js
@@ -54,6 +54,22 @@ async function run() {
       const result= await userCollection.insertOne(user)
       res.send(result)
     })
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id
+      const query= {_id: new ObjectId(id)}
+      const result= await userCollection.deleteOne(query)
+      res.send(result)
+    })
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id= req.params.id
+      const filter= {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: "admin"
+        }
+      };
+      const result= await userCollection.updateOne(filter, updateDoc)
+    })
 
     // menu related API
     app.get('/menu', async(req, res)=>{
