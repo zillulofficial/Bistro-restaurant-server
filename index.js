5f65ea8e20e4723ff92efc54fd1cae1c4c23cceb
zillulofficial <zillul.hakim100@gmail.com>
2025-01-02 09:58:44 +0600

    payment intent API created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
@@ -206,6 +207,22 @@ async function run() {
       const query = { _id: new ObjectId(id) }
       const result = await cartCollection.deleteOne(query)
       res.send(result)
+    })
+
+    // payment intent
+    app.post('/create-payment-intent', async(req, res)=>{
+      const {price}= req.body
+      const amount= parseInt(price * 100)
+
+      const paymentIntent= await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
